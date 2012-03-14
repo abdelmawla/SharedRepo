951a7f44a299bffee269fcf05b24a1245bf8d44f
Abdelmawla Mohamed <amohamed@integrant.com.jo>
2012-03-14 14:39:06 +0200

    Changed By Abdelmawla

--- SharedApplication/SharedApplication/Default.aspx.cs
+++ SharedApplication/SharedApplication/Default.aspx.cs
@@ -11,7 +11,7 @@ namespace SharedApplication
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            // added by amgad desouky @ 14/03/2011 2:21 pm
+            // Modified by Abdelmawla Mohamed @ 14/03/2011 2:21 pm
             if (!IsPostBack)
             {
             }
